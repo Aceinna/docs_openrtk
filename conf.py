7a11ce571fcaf47caf576cbe304105f937458622
Feng <feng@FengdeMacBook-Pro.local>
2018-06-18 16:24:16 -0700

    Changed name

--- conf.py
+++ conf.py
@@ -114,7 +114,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dmux81_user_guidedoc'
+htmlhelp_basename = 'OpenIMU'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -141,7 +141,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'dmux81_user_guide.tex', u'dmux81\\_user\\_guide Documentation',
+    (master_doc, 'OpenIMU.tex', u'OpenIMU Documentation',
      u'Feng', 'manual'),
 ]
 
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'dmux81_user_guide', u'dmux81_user_guide Documentation',
+    (master_doc, 'OpenIMU', u'OpenIMU Documentation',
      [author], 1)
 ]
 
@@ -162,7 +162,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'dmux81_user_guide', u'dmux81_user_guide Documentation',
-     author, 'dmux81_user_guide', 'One line description of project.',
+    (master_doc, 'OpenIMU', u'OpenIMU Documentation',
+     author, 'OpenIMU', 'One line description of project.',
      'Miscellaneous'),
 ]
